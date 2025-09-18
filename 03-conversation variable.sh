2fdd57bbbc2ee2445aab3579f20b412a62b0ee49
naginenihari <naginenihari@gmail.com>
2025-09-18 19:14:43 +0530

    conversation script created

--- 03-conversation variable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PERSON1=Hari
-PERSON2=Babu
-echo "$PERSON1:: How are you"
-echo "$PERSON2:: am fine what about you"
-echo "$PERSON1:: ok what are you doing am learning devsecops $PERSON2"
-echo "$PERSON2:: ok fine am also learning devsecops $PERSON1"
