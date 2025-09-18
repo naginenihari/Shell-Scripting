b285b23e65eb6d489375022cf134ddb8b5367161
naginenihari <naginenihari@gmail.com>
2025-09-18 18:50:36 +0530

    conversation script created

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: How are you"
+echo "Suresh:: am fine what about you"
+echo "Ramesh:: ok what are you doing am learning devsecops suresh"
+echo "Sures:: ok fine am also learning devsecops Ramesh"
