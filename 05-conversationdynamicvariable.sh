70279d4fa36efa484afbd94a1d8d480ea757dcae
naginenihari <naginenihari@gmail.com>
2025-09-19 18:08:44 +0530

    scripts are updated

---
+++ 05-conversationdynamicvariable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Arguments ,we are provide the deatils at runtime
+
+
+echo "enter your location pin number and code"
+
+Read -s PIN_NUMBER
+
+echo "your location pin number is :$PIN_NUMBER
+
