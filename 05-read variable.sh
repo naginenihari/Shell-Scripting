2528d0a6bd25412bcb7e5c377b871f009d2d39b9
naginenihari <naginenihari@gmail.com>
2025-09-19 18:48:59 +0530

    Environment variable updated

---
+++ 05-read variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# read means reading previous lime output and store -s mean hidden the read data
+
+
+echo "enter your location pin code number"
+
+read -s PIN_NUMBER
+
+echo "my location pin number and code is" :$PIN_NUMBER
