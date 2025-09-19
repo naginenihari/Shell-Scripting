4787184cfc1eac5f23ab5b041d871dc198d395da
naginenihari <naginenihari@gmail.com>
2025-09-19 17:34:35 +0530

    dynamic variable updated

---
+++ 04-conversationdynamicvariable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: am fine what about you $PERSON1"
+echo "$PERSON1:: what are you doing am learning devsecops $PERSON2"
+echo "$PERSON2:: fine am also learning devsecops $PERSON1"
