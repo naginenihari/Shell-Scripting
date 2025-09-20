dc46a870b16866dc79dd798823242c41c56497d2
naginenihari <naginenihari@gmail.com>
2025-09-20 21:01:40 +0530

    Special variables are updated

--- 08-1-SpecialVariable.sh
+++ 08-1-SpecialVariable.sh
@@ -5,3 +5,7 @@ echo "Display the script name: $0"
 echo "Present working directory: $PWD"
 echo "Current user name: $USER"
 echo "Current user home directory: $HOME"
+echo "PID of current script: $$"
+sleep 1
+echo "PID of backend proccess: $!"
+echo "Current user home directory: $?"
