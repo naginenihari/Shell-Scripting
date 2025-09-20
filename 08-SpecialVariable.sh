98bda3c7dc23f44bab2f68b9eb52b201a95e9258
naginenihari <naginenihari@gmail.com>
2025-09-20 20:34:27 +0530

    Special variables are updated

--- 08-SpecialVariable.sh
+++ 08-SpecialVariable.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 
-
-echo "All Variables passed to the script: $*"
-echo "All Variables passed to the script: $*"
-echo "All Variables passed to the script: $#"
+echo "All Variables passed to the script:$*"
+echo "All Variables passed to the script:$*"
+echo "Number of arguments passed to the script:$#"
