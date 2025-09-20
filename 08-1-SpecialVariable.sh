504505dc523510ab0c8a8e63e70b0c823969948c
naginenihari <naginenihari@gmail.com>
2025-09-20 20:53:37 +0530

    Special variables are updated

--- 08-1-SpecialVariable.sh
+++ 08-1-SpecialVariable.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "All Variables passed to the script:$1,$2,$3"
-echo "All Variables passed to the script:$*"
-echo "Number of arguments passed to the script:$#"
-echo "display the script name:$0"
+echo "All Variables passed to the script: $1,$2,$3"
+echo "Display the script name: $0"
+echo "Present working directory: $PWD"
+echo "Current user name: $USER"
+echo "Current user home directory: $HOME"
