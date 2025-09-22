307197c8124e7d5f9ec977b065cef3f0e7643539
naginenihari <naginenihari@gmail.com>
2025-09-22 15:39:15 +0530

    condition variable updated

--- 11-condition.sh
+++ 11-condition.sh
@@ -7,11 +7,8 @@
 
 NUMBER=$1
 
-if 
-    [ $NUMBER -lt 10 ];
-
-echo "given number $NUMBER is less then 10"
-
+if [ $NUMBER -lt 10 ];
+    echo "given number $NUMBER is less then 10"
 else 
     echo "given number $NUMBER is grater than 10"
 fi
