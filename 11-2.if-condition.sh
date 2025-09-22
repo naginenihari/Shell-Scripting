bb95f4c91d4d787de9a4fcdaf0250606acc07322
naginenihari <naginenihari@gmail.com>
2025-09-22 16:26:18 +0530

    condition variable updated

--- 11-2.if-condition.sh
+++ 11-2.if-condition.sh
@@ -2,7 +2,7 @@
     echo "enter the number "
     read NUMBER
 if ${[ $NUMBER %2 -eq 0 ]}; Then
-    echo given number $NUMBER is even
+    echo "given number $NUMBER is even"
 else
-    echo given number $NUMBER is odd
+    echo "given number $NUMBER is odd"
 fi
