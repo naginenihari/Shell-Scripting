25da0f094c10dafd947d2850d8b09833fc85cd96
naginenihari <naginenihari@gmail.com>
2025-09-22 19:09:02 +0530

    installation script updated

--- 12-Installation.sh
+++ 12-Installation.sh
@@ -4,3 +4,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run the script with root privelege"
 fi
+
+dnf install mysql -y
+
+if [$? -ne 0 ]; then
+    echo "ERROR:: installation of MYSQL is failure"
+else 
+    echo "installation of MYSQL is SUCCESS"
+fi
