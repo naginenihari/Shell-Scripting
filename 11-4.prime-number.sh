74159e29dab0e0c3aea78474ed8ce1224233fe3a
naginenihari <naginenihari@gmail.com>
2025-09-22 17:40:24 +0530

    if prime number condition updated

---
+++ 11-4.prime-number.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Enter a Number: "
+read -p NUMBER
+if [ "$NUMBER" -le 1 ]; then
+  echo "$NUMBER is not a prime number"
+  exit 0
+fi
+if [ "$NUMBER" -le 3 ]; then
+  echo "$NUMBER is a prime number"
+  exit 0
+fi
+if [ $((NUMBER % 2)) -eq 0 ]; then
+  echo "$NUMBER is not a prime number"
+  exit 0
+fi
+limit=$(echo "sqrt($NUMBER)" | bc)
+i=3
+while [ $i -le "$limit" ]
+do
+  if [ $((NUMBER % i)) -eq 0 ]; then
+    echo "$NUMBER is not a prime number"
+    exit 0
+  fi
+  i=$((i+2))
+done
+
+echo "$NUMBER is a prime number"
