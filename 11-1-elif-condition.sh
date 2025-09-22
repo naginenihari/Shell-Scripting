6355a85ff5bbed28dbf491787c9e88e085d056dc
naginenihari <naginenihari@gmail.com>
2025-09-22 16:08:20 +0530

    condition variable updated

--- 11-1-elif-condition.sh
+++ 11-1-elif-condition.sh
@@ -8,9 +8,9 @@
 NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
-    echo "given number $NUMBER is less then 10"
+    echo "given number $NUMBER is less then to 10"
 elif [ $NUMBER -eq 10 ]; then
-    echo "given number $NUMBER is equl 10"
+    echo "given number $NUMBER is equl to 10"
 else 
-    echo "given number $NUMBER is grater than 10"
+    echo "given number $NUMBER is grater than to 10"
 fi
--- 11-condition.sh
+++ 11-condition.sh
@@ -8,7 +8,7 @@
 NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
-    echo "given number $NUMBER is less then 10"
+    echo "given number $NUMBER is less then to 10"
 else 
-    echo "given number $NUMBER is grater than 10"
+    echo "given number $NUMBER is grater than to 10"
 fi
