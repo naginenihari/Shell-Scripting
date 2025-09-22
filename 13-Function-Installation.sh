946ddd9f57bce9bbd7e14daaa9219dd437ac1760
naginenihari <naginenihari@gmail.com>
2025-09-22 20:52:21 +0530

    installation of 3 softwares

---
+++ 13-Function-Installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root privelege"
+    exit 1  #failure mean other then Zero
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo "ERROR:: installation of $2 is failure"
+    exit 1
+else 
+    echo "installation of $2 is SUCCESS"
+fi
+ }
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+   
+dnf install nginx -y
+VALIDATE $? "NGINX"
+
+dnf install python3 -y
+VALIDATE $? "PYTHON"
