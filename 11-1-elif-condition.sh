3f51f997a8b6568ad29542449a4169f9b4d14fb7
naginenihari <naginenihari@gmail.com>
2025-09-22 15:59:20 +0530

    condition variable updated

---
+++ 11-1-elif-condition.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# -lt --less than
+# -gt -- greater than
+# -eq --equal
+# -ne --not equal
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ]; then
+    echo "given number $NUMBER is less then 10"
+elif
+    echo "given number $NUMBER is equl 10"
+else 
+    echo "given number $NUMBER is grater than 10"
+fi
