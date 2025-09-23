3098ae8ab521813e3aa402085011796ce0c8b7fa
naginenihari <naginenihari@gmail.com>
2025-09-23 15:01:18 +0530

    loops statement with packages updated

--- 18-loopspackage.sh
+++ 18-loopspackage.sh
@@ -9,6 +9,7 @@ N="\e[37m"
 LOGS_FOLDER="/var/log/shell-scripting"
 SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at:$(date)" |tee -a $LOG_FILE
 
@@ -16,6 +17,7 @@ if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
     exit 1  #failure mean other then Zero
 fi
+
 VALIDATE() {
 if [ $1 -ne 0 ]; then
     echo -e "installation of $2 is $R FAILURE $N" |tee -a $LOG_FILE
@@ -24,11 +26,14 @@ else
     echo -e "installation of $2 is $G SUCCESS $N" |tee -a $LOG_FILE
 fi
 }
+
 for package in $@
 do
-    if [ @? -ne 0 ]; then
-        dnf install $package -y &>>F$LOG_FILE
-    else
-        echo -e " $package already installed--$Y SKIPPING $N"
-    fi
+    dnf list installed $package -y &>>F$LOG_FILE
+ if [ @? -ne 0 ]; then
+    dnf install $package -y &>>F$LOG_FILE
+    VALIDATE $? "$package"    
+ else
+  echo -e " $package already installed--$Y SKIPPING $N"
+ fi
 done
