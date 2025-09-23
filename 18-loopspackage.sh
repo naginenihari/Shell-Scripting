5f0d356c2380fa8c2d3810baf9c7f0e0d7554837
naginenihari <naginenihari@gmail.com>
2025-09-23 15:21:23 +0530

    loops statement with packages updated

--- 18-loopspackage.sh
+++ 18-loopspackage.sh
@@ -30,7 +30,7 @@ fi
 for package in $@
 do
     dnf list installed $package &>>$LOG_FILE
- if [ @? -ne 0 ]; then
+ if [ $? -ne 0 ]; then
     dnf install $package -y &>>$LOG_FILE
     VALIDATE $? "$package"    
  else
