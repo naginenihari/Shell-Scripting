584a1d09f8a46fd6c6bba43793800b5c50c78401
naginenihari <naginenihari@gmail.com>
2025-09-23 14:13:17 +0530

    loops statement updated

---
+++ 18-loopspackage.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-scripting"
+SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
+    exit 1  #failure mean other then Zero
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e "installation of $2 is $R FAILURE $N" |tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e "installation of $2 is $G SUCCESS $N" |tee -a $LOG_FILE
+fi
+
+for package in $?
+do
+    echo "package is :$package"
+done
