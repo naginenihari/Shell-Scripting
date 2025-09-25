89a1a5007ebe7df359437fe15e81f4f6e388ad7e
naginenihari <naginenihari@gmail.com>
2025-09-25 17:29:57 +0530

    set.sh file updated

--- 19-set.sh
+++ 19-set.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-set -e
-hhh
-echo "error after mmm"
-hhkjaja;
-echo "error before"
+set -e 
+
+# error(){
+#     echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
+# }
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+echo "Hello.."
+echo "Before error.."
+ccaffjl;dnf # here shell understands there is an error and signal is ERR
+echo "After error"
