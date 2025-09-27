e5b98934e39983ebe18f545b729cb72a3bc69454
naginenihari <naginenihari@gmail.com>
2025-09-27 07:23:21 +0530

    whileloopscript is created

--- 23-whileloop.sh
+++ 23-whileloop.sh
@@ -4,7 +4,7 @@ count=10
 while [ $count -le 550 ]
 do
   echo "Current count: $count"
-  count=$((count + 10)) # Increment the counter
+  count=$((count + 100)) # Increment the counter
 done
 
 echo "Loop finished."
