275b150aa116525efc3506a979105228e781f3b8
naginenihari <naginenihari@gmail.com>
2025-09-27 10:29:18 +0530

    logback.sh file is created

--- 24-backup.sh
+++ 24-backup.sh
@@ -4,41 +4,40 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m"
+N="\e[37m"
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14} # if not provided considered as 14 days
 
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+LOGS_FOLDER="/var/log/shell-scripting"
+SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
+#Checking user is root user or not
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
+    echo "ERROR :: please run this scripts with root privelege"
+    exit 1
 fi
 
 USAGE(){
-    echo -e "$R USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days] $N"
+    echo -e "$R USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <NO_DAYS> $N"
     exit 1
 }
 
-### Check SOURCE_DIR and DEST_DIR passed or not ####
 if [ $# -lt 2 ]; then
-    USAGE
+    USAGE 
 fi
 
-### Check SOURCE_DIR Exist ####
 if [ ! -d $SOURCE_DIR ]; then
-    echo -e "$R Source $SOURCE_DIR does not exist $N"
+    echo -e "$R source $SOURCE_DIR is not exist $N"
     exit 1
 fi
 
-### Check DEST_DIR Exist ####
 if [ ! -d $DEST_DIR ]; then
-    echo -e "$R Destination $DEST_DIR does not exist $N"
+    echo -e "$R destination $DEST_DIR is not exist $N"
     exit 1
 fi
+
