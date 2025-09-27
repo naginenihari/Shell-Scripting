2fee188b23b67139ffbbc435f662f575a9faed42
naginenihari <naginenihari@gmail.com>
2025-09-27 11:19:24 +0530

    logback.sh file is created

--- 24-backup.sh
+++ 24-backup.sh
@@ -53,4 +53,4 @@ fi
 TIMESTAMP=$(date +%F-%H-%M)
 ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
  echo "Zip file name: $ZIP_FILE_NAME"
- echo $FILES |zip -@ -j "$ZIP_FILE_NAME"
+ echo "$FILES" |zip -@ -j "$ZIP_FILE_NAME"
