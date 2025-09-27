823423ec53fad7e020fae06cc028c9851c2a0be8
naginenihari <naginenihari@gmail.com>
2025-09-27 07:32:52 +0530

    whileloopscript is created

--- 23-whileloop.sh
+++ 23-whileloop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 count=10
-while [ $count -ge 550 ]
+while [ $count -le 550 ]
 do
   echo "Current count: $count"
   count=$((count + 100)) # Increment the counter
---
+++ Filenamereadloop.sh
@@ -0,0 +1,5 @@
+
+while IFS read -r line;
+do
+echo "proccessing line:$line"
+done <15-colours.sh
