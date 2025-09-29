7eba5d12b242b6d765a6a0d18d8009dcf186ad47
naginenihari <naginenihari@gmail.com>
2025-09-29 20:44:40 +0530

    email service installation

--- 25-1.diskusage.sh
+++ 25-1.diskusage.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 DISK_THRESHOLD=2
+DISK_USAGE=$(df -hT|grep -v Filesystem)
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+MESSAGE=""
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-MESSAGE=""
+MESSAGE=" "
 
 while IFS= read -r lines
-do
-  USAGE=$(echo $lines | awk '{print $6}' | cut -d '%' -f1)
-  PARTITION=$(echo $lines | awk '{print $7}')
+ do
+ USAGE=$(echo $lines |awk '{print $6}'| cut -d '%' -f1)
+ PARTITION=$(echo $lines |awk '{print $7}')
 
-  if [ "$USAGE" -gt "$DISK_THRESHOLD" ]; then
-    MESSAGE+="High usage on $PARTITION : $USAGE% \n"
-  fi
-done <<< "$DISK_USAGE"
+ if [ $USAGE -gt "$DISK_THRESHOLD" ]; then
+  MESSAGE+= "High usage on $PARTITION :$USAGE /n"
+ fi
 
-echo -e "Server IP: $IP_ADDRESS\n\nDisk Usage Report:\n$MESSAGE"
+ done <<< $DISK_USAGE
