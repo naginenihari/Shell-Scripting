0af128ff2b026dc9799c0bf98424bd39b10c89ea
naginenihari <naginenihari@gmail.com>
2025-09-29 20:24:36 +0530

    email service installation

--- 25-1.diskusage.sh
+++ 25-1.diskusage.sh
@@ -6,11 +6,11 @@ MESSAGE=""
 
 while IFS= read -r lines
  do
- USAGE=$(echo $lines |awk '{print$6}'| cut -d '%' -f1)
- PARTITION=$(echo $lines |awk '{print$7}')
+ USAGE=$(echo $lines |awk '{print $6}'| cut -d '%' -f1)
+ PARTITION=$(echo $lines |awk '{print $7}')
 
  if [ $USAGE -gt $DISK_THRESHOLD ]; then
-  MESSAGE= "High usage on $PARTITION :$USAGE"
+  MESSAGE+= "High usage on $PARTITION :$USAGE"
  fi
 
  done <<< $DISK_USAGE
