e51e8a247235c5188782d4b1622edda6a44458d3
naginenihari <naginenihari@gmail.com>
2025-09-29 20:31:31 +0530

    email service installation

--- 25-1.diskusage.sh
+++ 25-1.diskusage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DISK_THRESHOLD=2
-DISK_USAGE=$(df -hT|grep -v Filesystem)
-IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-MESSAGE=""
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=" "
 
 while IFS= read -r lines
  do
