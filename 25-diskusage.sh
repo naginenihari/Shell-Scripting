7c515a09c0979b5b5a65c9b2c898757417fcfba2
naginenihari <naginenihari@gmail.com>
2025-09-29 20:07:18 +0530

    email service installation

--- 25-diskusage.sh
+++ 25-diskusage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-DISK_USAGE=$(df -hT|grep -v filesystem|awk '{print$6}'|cut -d '%' -f1)
+DISK_THRESHOLD=2
+DISK_USAGE=$(df -hT|grep -v Filesystem)
 
 while IFS= read -r lines
  do
-
-echo "line $lines"
-
+ USAGE=$(echo $lines |awk '{print$6}'| cut -d '%' -f1)
+ PARTITION=$(echo $lines |awk '{print$7}')
+ if [$USAGE -gt $DISK_THRESHOLD]
+  echo "High usage on $PARTITION :$USAGE"
+ fi
  done <<< $DISK_USAGE
