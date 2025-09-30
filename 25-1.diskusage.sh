16baac5f8c854df350d9352a534530996bfa2378
naginenihari <naginenihari@gmail.com>
2025-09-30 11:24:36 +0530

    disk usage

--- 25-1.diskusage.sh
+++ 25-1.diskusage.sh
@@ -18,7 +18,7 @@ while IFS= read -r lines
  
  echo -e "message body:$MESSAGE"
  
-sh mail.sh "info@joindevops.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh "naginenihariaws@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
 
 # TO_ADDRESS=$1
 # SUBJECT=$2
